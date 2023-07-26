3ffb2eef223d52a0fe4862d1943aa312354ea4eb
Pychangeric <pychangeric@gmail.com>
2023-07-26 21:10:02 +0300

    done working with the artist model

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
   resources :playlists, only: [:create, :index, :show, :destory]
   resources :users
+  resources :music, only: [:index, :show, :create]
+  resources :artists
   post 'signup', to: 'users#create'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  
+  get '/musics', to: 'music#index'
+  post '/musics', to: 'music#create'
+  get '/artist', to: 'artist#index'
+  post '/artist', to: 'artist#create'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get '/musics', to: 'music#index'
 
 end
