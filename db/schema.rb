bfadf1752e1581dc779ecc9fc0fb793773b405ca
Pychangeric <pychangeric@gmail.com>
2023-07-26 10:23:27 +0300

    still working on the seding of the musics

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_26_064108) do
   create_table "artists", force: :cascade do |t|
     t.string "name"
     t.string "about"
