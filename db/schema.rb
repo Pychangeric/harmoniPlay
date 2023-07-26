09615dec59ce35a19cf7f1797b47dffbf32d8408
regina <reggiekariuki58@gmail.com>
2023-07-26 12:20:26 +0300

    changed the migrations and relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_25_111323) do
-  create_table "playlists", force: :cascade do |t|
-    t.string "title"
-    t.string "description"
-    t.binary "avatar"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "user_id"
-  end
-
-ActiveRecord::Schema[7.0].define(version: 2023_07_25_170205) do
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password_digest"
-    t.string "token"
-  end
+ActiveRecord::Schema[7.0].define(version: 2023_07_26_064108) do
   create_table "artists", force: :cascade do |t|
     t.string "name"
     t.string "about"
@@ -40,6 +24,23 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_25_170205) do
     t.string "genre"
     t.string "album"
     t.string "video"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "playlists", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.binary "avatar"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.string "token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
