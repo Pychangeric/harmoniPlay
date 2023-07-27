f62283a0a6e1e5d438c0b4e3e7b421b376e60d14
Max <maxmwangikenya@gmail.com>
2023-07-27 10:44:50 +0300

    worked on share models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+# Create some users
+user1 = User.create(username: 'user1', email: 'user1@example.com', password: 'password1')
+user2 = User.create(username: 'user2', email: 'user2@example.com', password: 'password2')
+# Create some songs
+song1 = Song.create(title: 'Song 1', artist: 'Artist 1', album: 'Album 1', user: user1)
+song2 = Song.create(title: 'Song 2', artist: 'Artist 2', album: 'Album 2', user: user2)
+# Create some shares
+Share.create(platform: 'Twitter', share_url: 'https://twitter.com/song1', user: user1, song: song1)
+Share.create(platform: 'Facebook', share_url: 'https://facebook.com/song1', user: user1, song: song1)
+Share.create(platform: 'WhatsApp', share_url: 'https://whatsapp.com/song2', user: user2, song: song2)
