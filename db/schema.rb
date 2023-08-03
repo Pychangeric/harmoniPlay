3d6a948411be84fff977dbd5e7d52645f3df85ac
belammuia <belammuia0@gmail.com>
2023-08-03 10:40:05 +0300

    changes to logout

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_26_165316) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_03_073600) do
   create_table "artists", force: :cascade do |t|
     t.string "name"
     t.string "about"
@@ -77,6 +77,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_26_165316) do
     t.string "token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "authentication_token"
+    t.index ["authentication_token"], name: "index_users_on_authentication_token"
   end
 
   add_foreign_key "artists", "musics"
