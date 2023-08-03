e42d38f4f4127b81c3ae00549cd4a4a06da1fba0
MunkushiR <ruth.munkushi@student.moringaschool.com>
2023-08-03 12:01:34 +0300

    done

--- db/schema.rb
+++ db/schema.rb
@@ -11,41 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_07_26_064247) do
-  create_table "recommendations", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_recommendations_on_user_id"
-  end
-
-  create_table "search_histories", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "query"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_search_histories_on_user_id"
-  end
-
-  add_foreign_key "recommendations", "users"
-  add_foreign_key "search_histories", "users"
-
-ActiveRecord::Schema[7.0].define(version: 2023_07_25_111323) do
-  create_table "playlists", force: :cascade do |t|
-    t.string "title"
-    t.string "description"
-    t.binary "avatar"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "user_id"
-  end
-
-ActiveRecord::Schema[7.0].define(version: 2023_07_25_170205) do
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password_digest"
-    t.string "token"
-  end
   create_table "artists", force: :cascade do |t|
     t.string "name"
     t.string "about"
@@ -63,4 +28,39 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_25_170205) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "playlists", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.binary "avatar"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
+  create_table "recommendations", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_recommendations_on_user_id"
+  end
+
+  create_table "search_histories", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "query"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_search_histories_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.string "token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "recommendations", "users"
+  add_foreign_key "search_histories", "users"
 end
