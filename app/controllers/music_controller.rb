c3d49369ca6756212ccdf2125d22848e18772df8
pychaneric <pychangeric@gmail.com>
2023-08-07 11:56:30 +0300

    changed the music

--- app/controllers/music_controller.rb
+++ app/controllers/music_controller.rb
@@ -6,10 +6,14 @@ class MusicController < ApplicationController
     render json: musics
   end
 
-  def show
-    render json: @music
-  end
-
+  def show 
+    playlist = Music.find_by(id:params[:id])
+    if playlist
+        render json: playlist
+    else 
+        render json: {error:"playlist not found"}
+    end
+end
   def create
     music = Music.new(music_params)
     if music.save
--- db/migrate/20230726061610_create_musics.rb
+++ db/migrate/20230726061610_create_musics.rb
@@ -9,9 +9,10 @@ class CreateMusics < ActiveRecord::Migration[7.0]
       
 
 
-      t.references :playlists, foreign_key: true
-      t.references :users, foreign_key: true
+      t.references :playlist, foreign_key: true
+      t.references :user, foreign_key: true
       t.references :artist, foreign_key: true
+      
 
 
 
